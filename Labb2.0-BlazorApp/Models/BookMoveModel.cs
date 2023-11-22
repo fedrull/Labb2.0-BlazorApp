956d3ff94af8bda5e323da44371b928a6037e817
Felix Magnusson <felix.magnusson01@gmail.com>
2023-11-22 14:04:26 +0100

    hej

---
+++ Labb2.0-BlazorApp/Models/BookMoveModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Labb2._0_BlazorApp.Models
+{
+	public class BookMoveModel
+	{
+		public int SourceStoreId { get; set; }
+		public string Title { get; set; }
+		public int Quantity { get; set; }
+		public int DestinationStoreId { get; set; }
+	}
+}
